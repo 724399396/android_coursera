60232c1bb14f32497d825c9cc76f68db813627a1
liww.li <liww.li@qunar.com>
2016-05-12 20:25:56 +0800

    lab5 test

---
+++ AsyncTaskLab/app/src/androidTest/java/coursera/labs/asynctasklab/FeedTest.java
@@ -0,0 +1,63 @@
+package coursera.labs.asynctasklab;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+
+import com.robotium.solo.Condition;
+import com.robotium.solo.Solo;
+
+public class FeedTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private Solo solo;
+
+    public FeedTest() {
+        super(MainActivity.class);
+    }
+
+    public void setUp() throws Exception {
+        solo = new Solo(getInstrumentation(), getActivity());
+        solo.setActivityOrientation(Solo.PORTRAIT);
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+    }
+
+    public void testRun() {
+        int shortDelay = 5000;
+        int longDelay = 10000;
+
+        // Wait for activity:
+        // 'TestFrontEndActivity'
+        solo.waitForActivity(MainActivity.class, shortDelay);
+
+        solo.waitForView(android.R.id.list);
+
+        solo.setActivityOrientation(Solo.LANDSCAPE);
+
+        solo.waitForActivity(MainActivity.class, shortDelay);
+
+        solo.waitForView(android.R.id.list);
+
+        final View listView = solo.getView(android.R.id.list);
+
+        solo.waitForCondition(new Condition() {
+            @Override
+            public boolean isSatisfied() {
+                return listView.isEnabled();
+            }
+        }, longDelay);
+
+        solo.clickOnView(solo.getView(android.R.id.text1));
+
+        // Assert that: 'feed_view' is shown
+        assertTrue("feed_view not shown!", solo.waitForView(solo
+                .getView(R.id.feed_view)));
+
+        // Assert that: 'Taylor Swift' is shown
+        assertTrue("'Taylor Swift' is not shown!",
+                solo.searchText("Taylor Swift"));
+    }
+}
+
